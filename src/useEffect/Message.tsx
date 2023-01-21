28c8889cbfaa394962b5884f17061c55760a1a0b
Julian Celeita <andresceleita@yahoo.es>
2023-01-21 14:55:13 -0500

    coords with useEffect, eventListener

--- src/useEffect/Message.tsx
+++ src/useEffect/Message.tsx
@@ -1,18 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Message = () => {
-
+  
+  const [coords, setCoords] = useState( {x:0, y:0});
+    
     useEffect(() => {
-      console.log('Message enabled');
+
+      const onMouseMove = ({x, y}:{x:number, y:number}) => {
+        setCoords ({ x, y })
+      }
+
+      window.addEventListener( 'mousemove', onMouseMove );
+
+        /* window.addEventListener ( 'mousemove', (event) =>{
+            console.log(event);
+        } ) */
+        //console.log('Message enabled');
     
       return () => {
-        console.log('UPS Message disabled');        // Al quitar el mensaje, se desmonta el componente.
+
+        window.removeEventListener( 'mousemove', onMouseMove );
+
+        //console.log('UPS Message disabled');        // Al quitar el mensaje, se desmonta el componente.
       }
-    }, [])
+    }, []);
     
   return (
     <>
     <h3> Usuario ya en uso </h3>
+    {JSON.stringify( coords )}                {/* Para mostrar un objeto en html */}
     </>
     )
 }
