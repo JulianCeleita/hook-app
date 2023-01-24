12ae7de3edf4e7304876700e1b11851885646ef9
Julian Celeita <andresceleita@yahoo.es>
2023-01-24 16:29:17 -0500

    new quote and button to next

--- src/examples/MultipleCustomHooks.tsx
+++ src/examples/MultipleCustomHooks.tsx
@@ -1,17 +1,29 @@
-import { useFetch } from '../hooks/useFetch';
+import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
+  const { data, isLoading, hasError } = useFetch(
+    "https://api.breakingbadquotes.xyz/v1/quotes"
+  );
 
-    const { data, isLoading, hasError } = useFetch ('https://api.breakingbadquotes.xyz/v1/quotes');
-
-    console.log({ data, isLoading, hasError})
+    const { author, quote } = !!data && data[0];      // doble negación: ! = false, !! = true desestructuro el author y quote de data, si data es true, entonces muestre el primer valor [0] 
 
   return (
     <>
-            <h1> BreakingBad Quotes</h1>
+      <h1> BreakingBad Quotes</h1>
 
-            <hr />
+      <hr />
 
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1"> { quote } </p>
+          <footer className="blockquote-footer"> {author} </footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary"> 
+      Next quote </button>
     </>
-  )
-}
+  );
+};
