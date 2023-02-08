bec6ad6c744e29db18db2e55495eea456faa7242
Julian Celeita <andresceleita@yahoo.es>
2023-02-07 21:11:15 -0500

    new TODO add button and delete items

--- src/useReducer/TodoApp.tsx
+++ src/useReducer/TodoApp.tsx
@@ -1,38 +1,53 @@
-import { useReducer } from "react"
-import { todoReducer } from './todoReducer';
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
 const initialState = [
-    {
-        id: new Date().getTime(),
-        description: 'Ir a Pandala',
-        done: false,
-    },
+  {
+    id: new Date().getTime(),
+    description: "Ir a Pandala",
+    done: false,
+  },
 
-    {
-        id: new Date().getTime() * 3,
-        description: 'Ir a Pandala',
-        done: false,
-    }
-]
+  {
+    id: new Date().getTime() * 3,
+    description: "Ir a Pandala",
+    done: false,
+  },
+];
 
 export const TodoApp = () => {
-
-    const [todos, dispatch] = useReducer( todoReducer, initialState )
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
   return (
     <>
-        <h1> Todo App</h1>
-        <hr />
+      <h1> Todo App</h1>
+      <hr />
 
-        <ul>
-            <li> Item 1 </li>
-            <li> Item 2 </li>
-            <li> Item 3 </li>
-        </ul>
+      <div className="row">
+        <div className="col">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between"> 
+                <span className="align-self-center">Item 1 </span>
+                <button className="btn btn-danger">Delete</button>
+            </li>
+          </ul>
+        </div>
 
-
+        <div className="col">
+          <h4>Add To Do</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="What is there to do?"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-primary mt-3">
+              Add
+            </button>
+          </form>
+        </div>
+      </div>
     </>
-
-
-    )
-}
+  );
+};
