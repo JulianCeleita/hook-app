0f9ae1dc6819b02f91bb6951074b661e60f7059d
Julian Celeita <andresceleita@yahoo.es>
2023-02-12 21:38:40 -0500

    New routes created

--- src/useContext/MainApp.tsx
+++ src/useContext/MainApp.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { HomePage, LoginPage, AboutPage } from './';
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
+
+      <Routes>
+        <Route path="/" element={ <HomePage/> } />
+        <Route path="about" element={ <AboutPage/> } />
+        <Route path="login" element={ <LoginPage/> } />
+      </Routes>
     </>
   );
 };
